47fa7e70a7015c2609b7a098187e0151a9e93b4d
Kevin Dela Paz <kevindelapaz21@gmail.com>
2024-11-22 13:23:28 -0600

    week 0 done

---
+++ week-0-querying/players/10.sql
@@ -0,0 +1,3 @@
+SELECT first_name, last_name, birth_year AS "Year Born" FROM players
+WHERE bats = "R"
+ORDER BY 'Year Born';
---
+++ week-0-querying/players/7.sql
@@ -0,0 +1,3 @@
+SELECT count(*) FROM players
+WHERE bats = 'R' AND throws = 'L' 
+OR bats = 'L' AND throws = 'R';
