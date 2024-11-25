f2666d016142931afcd0da3193ea328ca8431be8
Kevin Dela Paz <kevindelapaz21@gmail.com>
2024-11-24 21:23:19 -0600

    halfway done problem set 1

--- week-1-relating/packages/log.sql
+++ week-1-relating/packages/log.sql
@@ -1,7 +1,16 @@
 
 -- *** The Lost Letter ***
+SELECT id FROM addresses WHERE address = '900 Somerville Avenue';
+SELECT * FROM packages WHERE from_address_id = 432;
+SELECT * FROM scans WHERE package_id = 384;
+SELECT * FROM addresses WHERE id = 854;
 
 -- *** The Devious Delivery ***
+SELECT * FROM packages WHERE from_address_id IS NULL;
+SELECT * FROM scans WHERE package_id IN ( 
+    SELECT id FROM packages WHERE from_address_id IS NULL
+);
+SELECT * FROM addresses WHERE id = 348;
 
 -- *** The Forgotten Gift ***
 
